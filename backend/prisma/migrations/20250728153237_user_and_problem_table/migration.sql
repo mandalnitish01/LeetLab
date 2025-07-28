a126583cdd0db6b65b52f3aa9efe6bc84985148a
nitish <mandalnitish570@gmail.com>
2025-07-28 21:09:22 +0530

    problem model is added

---
+++ backend/prisma/migrations/20250728153237_user_and_problem_table/migration.sql
@@ -0,0 +1,41 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - Made the column `image` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- CreateEnum
+CREATE TYPE "Difficulty" AS ENUM ('EASY', 'MEDIUM', 'HARD');
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "image" SET NOT NULL,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- CreateTable
+CREATE TABLE "Problem" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "difficulty" "Difficulty" NOT NULL,
+    "tags" TEXT[],
+    "examples" JSONB NOT NULL,
+    "constraints" TEXT NOT NULL,
+    "hints" TEXT,
+    "editorial" TEXT,
+    "testCases" JSONB NOT NULL,
+    "codeSnippets" JSONB NOT NULL,
+    "referenceSolutions" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Problem_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Problem" ADD CONSTRAINT "Problem_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
