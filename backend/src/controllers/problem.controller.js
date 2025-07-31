f808eb4ff1e10b00f64949aaf2d9cc77be1cd4be
nitish <mandalnitish570@gmail.com>
2025-07-31 15:27:51 +0530

    complete one more route that is createProblem but the testing is missing. i'll check this route in next commit probably.

--- backend/src/controllers/problem.controller.js
+++ backend/src/controllers/problem.controller.js
@@ -1,4 +1,101 @@
-export const createProblem = async (req, res) => {}
+import {db} from '../libs/db.js'
+import { getJudge0LanguageId, pollBatchResults, submitBatch } from '../libs/judge0.lib.js';
+
+
+
+export const createProblem = async (req, res) => {
+    //geting/extracting all the data from the request body
+    const { title,description,difficulty,tags,examples,constraints,testCases,codeSnippets,referenceSolutions } = req.body;
+
+    //check the user role
+    if (req.user.role !== 'ADMIN') {
+        return res.status(403).json({ message: 'Access denied. Only admins can create problems.' });
+    }
+    //loop through all the referance solutions for different languages
+    try {
+        for(const [language , solutionCode] of Object.entries(referenceSolutions)){
+            //extract the language id from the solution code getJudge0LanguageId function
+            const languageId = getJudge0LanguageId(language);
+
+            //through an error if the language is not supported
+            if (!languageId) {
+                return res.status(400).json({ message: `Unsupported language: ${language}` });
+            }
+
+            const submissions = testCases.map((input , output) =>{
+                return {
+                    language_id: languageId,
+                    source_code: solutionCode,
+                    stdin: input,
+                    expected_output: output
+                };
+            });
+            //extract all the submission results tokens
+            const submissionResults = await submitBatch(submissions);
+
+            //get all the tokens from the submission results
+            const tokens = submissionResults.map((res) => res.token);
+
+            //create a pooling of the problem with the tokens
+            const results = await pollBatchResults(tokens)
+
+
+            //Check if the submission was successful
+            for(let i=0; i < results.length; i++) {
+                const result = results[i];
+                if (result.status.id !== 3) { // Check if the submission was successful
+                    return res.status(400).json({ error:`Test case ${i+1} failed for language ${language}` });
+                }
+            }
+
+            //save the problem to the DataBase
+            const newProblem = await db.problem.create({
+data:{
+    title,
+    description,
+    difficulty,
+    tags,
+    examples,
+    constraints,
+    testCases,
+    codeSnippets,
+    referenceSolutions,
+    userId: req.user.id, // Assuming you want to associate the problem with the user who created it
+    // languageId, // Save the language ID for the problem
+}
+                // data: {
+                //     title,
+                //     description,
+                //     difficulty,
+                //     tags,
+                //     examples,
+                //     constraints,
+                //     testCases,
+                //     codeSnippets: {
+                //         create: codeSnippets.map((codeSnippet) => ({
+                //             language,
+                //             code: codeSnippet
+                //         }))
+                //     },
+                //     referenceSolutions: {
+                //         create: {
+                //             language,
+                //             solutionCode
+                //         }
+                //     }
+                // }
+            });
+
+            
+            return res.status(201).json(newProblem);
+            
+        }
+        
+    } catch (error) {
+        
+    }
+
+}
 
 export const getAllProblems = async (req, res) => {}
 export const getProblemById = async (req, res) => {}
---
+++ backend/src/libs/judge0.lib.js
@@ -0,0 +1,38 @@
+export const getJudge0LanguageId = (language) => {
+    const languageMap = {
+        'python': 34,
+        'javascript': 63,
+        'java': 62,
+        'c++': 54}
+    return languageMap[language.toUpperCase()];
+}
+
+export const submitBatch = async (submissions) => {
+    const {data} = await axios.post(`${process.env.JUDGE0_API_URL}submissions/batch?base64_encoded=false`, {
+        submissions
+    });
+    console.log("Submission results:", data);
+    return data; //[{tpokens},{token},{token}];
+
+}
+
+
+const sleep  = (ms) => {return new Promise((resolve) => setTimeout(resolve, ms));}
+export const pollBatchResults = async (tokens)=>{
+while(true){
+    const {data} = await axios.get(`${process.env.JUDGE0_API_URL}submissions/batch`,{
+        params: {
+            tokens: tokens.join(','),
+            base64_encoded: false
+        }
+    });
+    const results = data.submissions;
+    const isAllDone = results.every((result) =>
+         result.status.id !== 1 && result.status.id !== 2
+    );  
+    if (isAllDone) {
+        return results;
+    }
+    await sleep(1000); // Wait for 1 second before polling again
+}
+}
