2bc50aed344085a438d011d9d37bfb65fdeafd48
nitish <mandalnitish570@gmail.com>
2025-08-07 20:52:29 +0530

    Problem created successfully in DB. I spent 2-3 hours on this to find the error, the error is destructure the map arguements, i forgot to destructure. debugging is not that easy compare to coding.

--- backend/src/controllers/problem.controller.js
+++ backend/src/controllers/problem.controller.js
@@ -1,86 +1,138 @@
-import {db} from '../libs/db.js'
-import { getJudge0LanguageId, pollBatchResults, submitBatch } from '../libs/judge0.lib.js';
-
-
+import { db } from "../libs/db.js";
+import {
+  getJudge0LanguageId,
+  pollBatchResults,
+  submitBatch,
+} from "../libs/judge0.lib.js";
 
 export const createProblem = async (req, res) => {
-    //geting/extracting all the data from the request body
-    const { title,description,difficulty,tags,examples,constraints,testCases,codeSnippets,referenceSolutions } = req.body;
-console.log("Problem set" , req.body);
-    //check the user role
-    if (req.user.role !== 'ADMIN') {
-        return res.status(403).json({ message: 'Access denied. Only admins can create problems.' });
-    }
-    //loop through all the referance solutions for different languages
-    try {
-        for(const [language , solutionCode] of Object.entries(referenceSolutions)){
-            //extract the language id from the solution code getJudge0LanguageId function
-            const languageId = getJudge0LanguageId(language);
-            console.log(languageId)
-            //through an error if the language is not supported
-            if (!languageId) {
-                return res.status(400).json({ message: `Unsupported language: ${language}` });
-            }
+  //geting/extracting all the data from the request body
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    examples,
+    constraints,
+    testCases,
+    codeSnippets,
+    referenceSolutions,
+  } = req.body;
+  console.log("Problem set----------", req.body);
+  //check the user role
+  if (req.user.role !== "ADMIN") {
+    return res
+      .status(403)
+      .json({ message: "Access denied. Only admins can create problems." });
+  }
+  //loop through all the referance solutions for different languages
+  try {
+    for (const [language, solutionCode] of Object.entries(referenceSolutions)) {
+      //extract the language id from the solution code getJudge0LanguageId function
+      const languageId = getJudge0LanguageId(language);
+      //   console.log(languageId);
+      //through an error if the language is not supported
+      if (!languageId) {
+        return res
+          .status(400)
+          .json({ message: `Unsupported language: ${language}` });
+      }
 
-            const submissions = testCases.map((input , output) =>{
-                return {
-                    language_id: languageId,
-                    source_code: solutionCode,
-                    stdin: input,
-                    expected_output: output
-                };
-            });
-            //extract all the submission results tokens
-            const submissionResults = await submitBatch(submissions);
-console.log("submission Results data " , submissionResults)
-            //get all the tokens from the submission results
-            const tokens = submissionResults.map((res) => res.token);
-console.log("Token for checking the results", tokens)
-            //create a pooling of the problem with the tokens
-            const results = await pollBatchResults(tokens)
-console.log("result after polling", results)
+      //yaadgaar problem find that is i forget to destructure the arguements
+    //   i spent 2-3 hours on this to find the error
+      const submissions = testCases.map(({input, output}) => {
+        console.log(
+          "Language ID-----",
+          languageId,
+          "Solution Code------",
+          solutionCode,
+          "Input -------",
+          input,
+          "Output -----",
+          output
+        );
+        return {
+          source_code: solutionCode,
+          language_id: languageId,
+          stdin: input,
+          expectedOutput: output,
+        };
+      });
+      //extract all the submission results tokens
+      const submissionResults = await submitBatch(submissions);
+      // console.log("submission Results data " , submissionResults)
+      //get all the tokens from the submission results
+      const tokens = submissionResults.map((res) => res.token);
+      // console.log("Token for checking the results", tokens)
+      //create a pooling of the problem with the tokens
+      const results = await pollBatchResults(tokens);
+      // console.log("result after polling", results)
 
-            //Check if the submission was successful
-            for(let i=0; i < results.length; i++) {
-                const result = results[i];
-                console.log("result-------" , result)
-                if (result.status.id !== 3) { // Check if the submission was successful
-                    return res.status(400).json({ error:`Test case ${i+1} failed for language ${language}` });
-                }
-            }
+      //Check if the submission was successful
+      //   for (let i = 0; i < results.length; i++) {
+      //     const result = results[i];
+      //     console.log("result-------", result);
+      //     if (result.status.id !== 3) {
+      //       // Check if the submission was successful
+      //       return res
+      //         .status(400)
+      //         .json({
+      //           error: `Test case ${i + 1} failed for language ${language}`,
+      //         });
+      //     }
+      //   }
+      for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+        console.log("result-----------", result);
+        const expectedOutput = testCases[i].output.trim();
+        const actualOutput = result.stdout?.trim();
 
-            //save the problem to the DataBase
-            const newProblem = await db.Problem.create({
-                data: {
-                    title,
-                    description,
-                    difficulty,
-                    tags,
-                    examples,
-                    constraints,
-                    testCases,
-                    codeSnippets,
-                    referenceSolutions,
-                    userId: req.user.id, // Assuming you want to associate the problem with the user who created it
-                    // languageId, // Save the language ID for the problem
-}});   
-// console.log("Problem created successfully but no", newProblem)         
-    return res.status(201).json({
-        success: true,        
-        message: 'Problem created successfully',
-        Problem: newProblem
-        
-    });  
+        console.log(`--- Test Case ${i + 1} ---`);
+        console.log("Expected:", JSON.stringify(expectedOutput));
+        console.log("Actual  :", JSON.stringify(actualOutput));
+
+        if (result.status.id !== 3 || actualOutput !== expectedOutput) {
+          return res.status(400).json({
+            error: `Test case ${i + 1} failed for language ${language}`,
+            actual: actualOutput,
+            expected: expectedOutput,
+          });
         }
-        
-    } catch (error) {
-        
+      }
+
+      //save the problem to the DataBase
+      const newProblem = await db.Problem.create({
+        data: {
+          title,
+          description,
+          difficulty,
+          tags,
+          examples,
+          constraints,
+          testCases,
+          codeSnippets,
+          referenceSolutions,
+          userId: req.user.id, // Assuming you want to associate the problem with the user who created it
+          // languageId, // Save the language ID for the problem
+        },
+      });
+      // console.log("Problem created successfully but no", newProblem)
+      return res.status(201).json({
+        success: true,
+        message: "Problem created successfully",
+        Problem: newProblem,
+      });
     }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Error While Creating Problem",
+    });
+  }
+};
 
-}
-
-export const getAllProblems = async (req, res) => {}
-export const getProblemById = async (req, res) => {}
-export const updateProblem = async (req, res) => {}
-export const deleteProblem = async (req, res) => {}
-export const getAllProblemsSolvedByUser = async (req, res) => {}
+export const getAllProblems = async (req, res) => {};
+export const getProblemById = async (req, res) => {};
+export const updateProblem = async (req, res) => {};
+export const deleteProblem = async (req, res) => {};
+export const getAllProblemsSolvedByUser = async (req, res) => {};
--- backend/src/libs/judge0.lib.js
+++ backend/src/libs/judge0.lib.js
@@ -1,32 +1,36 @@
+import axios from "axios";
 export const getJudge0LanguageId = (language) => {
     const languageMap = {
-        'PYTHON': 34,
-        'JAVASCRIPT': 63,
-        'JAVA': 62,
-        'C++': 54
+        "PYTHON": 71,
+        "JAVA": 62,
+        "JAVASCRIPT": 63
     };
     return languageMap[language.toUpperCase()];
 };
 
+const sleep  = (ms)=> new Promise((resolve)=> setTimeout(resolve , ms))
+
 export const submitBatch = async (submissions) => {
+    console.log("url-----",`${process.env.JUDGE0_API_URL}submissions/batch?base64_encoded=false`, {
+        submissions
+    })
     const {data} = await axios.post(`${process.env.JUDGE0_API_URL}submissions/batch?base64_encoded=false`, {
         submissions
     });
     console.log("Submission results:", data);
-    return data; //[{tpokens},{token},{token}];
-
+    return data; //[{token},{token},{token}];
 }
 
 
-const sleep  = (ms) => {return new Promise((resolve) => setTimeout(resolve, ms));}
 export const pollBatchResults = async (tokens)=>{
 while(true){
     const {data} = await axios.get(`${process.env.JUDGE0_API_URL}submissions/batch`,{
         params: {
-            tokens: tokens.join(','),
+            tokens: tokens.join(","),
             base64_encoded: false
         }
     });
+
     const results = data.submissions;
     const isAllDone = results.every((result) =>
          result.status.id !== 1 && result.status.id !== 2
