f4b8951bc524f1b04af890bfd540238e947820c6
nitish <mandalnitish570@gmail.com>
2025-08-12 20:26:10 +0530

    create submission route and write controller for it.

---
+++ backend/src/controllers/submission.controller.js
@@ -0,0 +1,54 @@
+import {} from '../libs/db.js'
+
+export const getAllSubmission = async(req, res)=>{
+    try {
+        const userId = req.user.id;
+        const submissions = await db.submission.findMany({
+            where: {
+                userId:userId
+            }
+        })
+        res.status(200).json({success:true, message:"Submissions Fetched Successfully", submissions})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error:"Error While Fetching Submissions"})
+        
+    }
+    
+}
+
+export const getSubmissionsForProblem = async(req, res)=>{
+    try {
+        const userId = req.user.id;
+        const problemId = req.params.problemId;
+        const submission = await db.submission.findMany({
+            where:{
+                userId:userId,
+                problemId:problemId
+            }
+        })
+        res.status(200).json({success:true, message:"Submissions Fetched Successfully", submission})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error:"Error While Fetching Submissions"})
+        
+    }
+    
+}
+
+export const getAllTheSubmissionsForProblem = async(req, res)=>{
+    try {
+        const problemId = req.params.problemId;
+        const submission = await db.submission.count({
+            where:{
+                problemId:problemId
+            }
+        })
+        res.status(200).json({success:true, message:"Submissions Fetched Successfully", submission})
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error:"Error While Fetching Submissions"})
+        
+    }
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,7 @@ import cookParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import problemRoutes from "./routes/problem.routes.js";
 import executionRoutes from "./routes/executeCode.routes.js";
+import submissionRoutes from "./routes/submission.routes.js";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -22,6 +23,7 @@ app.use(cookParser());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/problems", problemRoutes);
 app.use("/api/v1/execute-code", executionRoutes);
+app.use("/api/v1/submission", submissionRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
