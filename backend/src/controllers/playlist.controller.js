1774f048c15b35089a52db433f4b4241b31c225e
nitish <mandalnitish570@gmail.com>
2025-09-02 18:08:10 +0530

    createplaylist controller inside the playlist.controller.js

--- backend/src/controllers/playlist.controller.js
+++ backend/src/controllers/playlist.controller.js
@@ -1,7 +1,38 @@
-export const createPlaylist = async (req,res)=>{}
-export const getAllListDetails = async (req,res)=>{}
-export const getPlayListDetails = async (req,res)=>{}
-export const addProblemToPlaylist = async (req,res)=>{}
-export const deletePlaylist = async (req,res)=>{}
-export const removeProblemFromPlaylist = async (req,res)=>{}
+import db from "../utils/db.server.js";
 
+export const createPlaylist = async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    const userId = req.user.id;
+    //validation
+    if (!name) {
+      return res.status(400).json({ error: "Playlist name is required" });
+    }
+    //create playlist
+    const playlist = await db.playlist.create({
+      data: {
+        name,
+        description,
+        userId,
+      },
+    });
+    //send response
+    return res
+      .status(201)
+      .json({
+        success: true,
+        message: "Playlist Created Successfully",
+        playlist
+      });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Error While Creating Playlist" });
+  }
+};
+export const getAllListDetails = async (req, res) => {
+  
+};
+export const getPlayListDetails = async (req, res) => {};
+export const addProblemToPlaylist = async (req, res) => {};
+export const deletePlaylist = async (req, res) => {};
+export const removeProblemFromPlaylist = async (req, res) => {};
